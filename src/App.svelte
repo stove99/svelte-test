06bd563ef5be60713bc70c38dd5d71a118ecb0cd
김영명 <ymkim@onmakers.com>
2020-05-20 17:57:16 +0900

    css

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<style>
+  div {
+    border: 1px solid #ccc;
+  }
+</style>
+
+<h1>테스트</h1>
+
+<div bp="grid 6@md">
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+</div>
